ad57adbd53f77a780ab69ca741d9bf0e9e585d30
Eric Shamow <eric@puppetlabs.com>
2011-08-20 17:16:23 -0400

    Added centos56-amd64, removed i386 for now

--- centos56-amd64-412/definition.rb
+++ centos56-amd64-412/definition.rb
@@ -1,13 +1,12 @@
 Veewee::Session.declare({
   :cpu_count => '1', :memory_size=> '384',
   :disk_size => '10140', :disk_format => 'VDI', :hostiocache => 'off', :ioapic => 'on', :pae => 'on',
-  :os_type_id => 'RedHat',
-  :iso_file => "CentOS-5.6-i386-bin-DVD.iso", :iso_src => "", :iso_md5 => "181142a5845586579d5d4d96e43a4827", :iso_download_timeout => 1000,
-  :iso_download_instructions => "We can not download the ISO , you need to download it yourself and put it in the iso directory\n"+
-  "- URL: http://isoredirect.centos.org/centos/5/isos/i386/ ",
-  :boot_wait => "10", :boot_cmd_sequence => [
-    'linux text ks=http://%IP%:%PORT%/ks.cfg<Enter>'
-  ],
+  :os_type_id => 'RedHat_64',
+  :iso_file => "CentOS-5.6-x86_64-netinstall.iso",
+  :iso_src => "http://mirror.ukhost4u.com/centos/5.6/isos/x86_64/CentOS-5.6-x86_64-netinstall.iso",
+  :iso_md5 => "02cf3a5e32aaa5eed27af775ad292beb",
+  :iso_download_timeout => 1000,
+  :boot_wait => "10", :boot_cmd_sequence => [ 'linux text ks=http://%IP%:%PORT%/ks.cfg<Enter>' ],
   :kickstart_port => "7122", :kickstart_timeout => 10000, :kickstart_file => "ks.cfg",
   :ssh_login_timeout => "100", :ssh_user => "vagrant", :ssh_password => "vagrant", :ssh_key => "",
   :ssh_host_port => "7222", :ssh_guest_port => "22",
--- centos56-amd64-412/postinstall.sh
+++ centos56-amd64-412/postinstall.sh
@@ -1,19 +1,22 @@
 #http://chrisadams.me.uk/2010/05/10/setting-up-a-centos-base-box-for-development-and-testing-with-vagrant/
-#kernel source is needed for vbox additions
 
 date > /etc/vagrant_box_build_time
 
-rpm -Uvh http://download.fedora.redhat.com/pub/epel/5/i386/epel-release-5-4.noarch.rpm
+fail()
+{
+  echo "FATAL: $*"
+  exit 1
+}
 
-yum -y install gcc bzip2 make kernel-devel-`uname -r` git
+rpm -Uvh http://download.fedora.redhat.com/pub/epel/5/x86_64/epel-release-5-4.noarch.rpm
 
+#kernel source is needed for vbox additions
+yum -y install gcc bzip2 make kernel-devel-`uname -r`
 #yum -y update
 #yum -y upgrade
 
-yum -y install gcc-c++ zlib-devel openssl-devel readline-devel sqlite3-devel
-
+yum -y install gcc-c++ zlib-devel openssl-devel readline-devel sqlite3-devel git
 yum -y erase wireless-tools gtk2 libX11 hicolor-icon-theme avahi freetype bitstream-vera-fonts
-
 yum -y clean all
 
 #Installing ruby
@@ -25,18 +28,19 @@ rm -rf ./ruby-enterprise-1.8.7-2010.02/
 rm ruby-enterprise-1.8.7-2010.02.tar.gz
 
 #Installing chef & Puppet
-#/opt/ruby/bin/gem install chef --no-ri --no-rdoc
-#/opt/ruby/bin/gem install puppet --no-ri --no-rdoc
+echo "Installing chef and puppet"
+#/usr/local/bin/gem install chef --no-ri --no-rdoc || fail "Could not install chef"
+#/usr/local/bin/gem install puppet --no-ri --no-rdoc || fail "Could not install puppet"
 
 # Get envpuppet install script
 wget http://192.168.0.195/~eric/isos/puppetinst.sh
-sh puppetinst_debian.sh
+sh puppetinst.sh
 
 #Installing vagrant keys
 mkdir /home/vagrant/.ssh
 chmod 700 /home/vagrant/.ssh
 cd /home/vagrant/.ssh
-wget http://192.168.0.195/~eric/isos/vagrant.pub -O authorized_keys
+wget --no-check-certificate 'http://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub' -O authorized_keys
 chown -R vagrant /home/vagrant/.ssh
 
 #Installing the virtualbox guest additions
@@ -48,6 +52,7 @@ sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
 
 rm VBoxGuestAdditions_$VBOX_VERSION.iso
+
 
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 
