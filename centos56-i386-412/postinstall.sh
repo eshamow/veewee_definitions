3519fffc79cdd6d8ad7f487b78e48c8ef1f1af3d
Eric Shamow <eric@puppetlabs.com>
2011-08-20 12:58:24 -0400

    New CentOS build

--- centos56-i386-412/postinstall.sh
+++ centos56-i386-412/postinstall.sh
@@ -3,7 +3,9 @@
 
 date > /etc/vagrant_box_build_time
 
-yum -y install gcc bzip2 make kernel-devel-`uname -r`
+rpm -Uvh http://download.fedora.redhat.com/pub/epel/5/i386/epel-release-5-4.noarch.rpm
+
+yum -y install gcc bzip2 make kernel-devel-`uname -r` git
 
 #yum -y update
 #yum -y upgrade
@@ -15,7 +17,7 @@ yum -y erase wireless-tools gtk2 libX11 hicolor-icon-theme avahi freetype bitstr
 yum -y clean all
 
 #Installing ruby
-wget http://rubyforge.org/frs/download.php/71096/ruby-enterprise-1.8.7-2010.02.tar.gz
+wget http://192.168.0.195/~eric/isos/ruby-enterprise-1.8.7-2010.02.tar.gz
 tar xzvf ruby-enterprise-1.8.7-2010.02.tar.gz
 ./ruby-enterprise-1.8.7-2010.02/installer -a /opt/ruby --no-dev-docs --dont-install-useful-gems
 echo 'PATH=$PATH:/opt/ruby/bin'> /etc/profile.d/rubyenterprise.sh
@@ -23,20 +25,24 @@ rm -rf ./ruby-enterprise-1.8.7-2010.02/
 rm ruby-enterprise-1.8.7-2010.02.tar.gz
 
 #Installing chef & Puppet
-/opt/ruby/bin/gem install chef --no-ri --no-rdoc
-/opt/ruby/bin/gem install puppet --no-ri --no-rdoc
+#/opt/ruby/bin/gem install chef --no-ri --no-rdoc
+#/opt/ruby/bin/gem install puppet --no-ri --no-rdoc
+
+# Get envpuppet install script
+wget http://192.168.0.195/~eric/isos/puppetinst.sh
+sh puppetinst_debian.sh
 
 #Installing vagrant keys
 mkdir /home/vagrant/.ssh
 chmod 700 /home/vagrant/.ssh
 cd /home/vagrant/.ssh
-wget --no-check-certificate 'http://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub' -O authorized_keys
+wget http://192.168.0.195/~eric/isos/vagrant.pub -O authorized_keys
 chown -R vagrant /home/vagrant/.ssh
 
 #Installing the virtualbox guest additions
 VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
 cd /tmp
-wget http://download.virtualbox.org/virtualbox/$VBOX_VERSION/VBoxGuestAdditions_$VBOX_VERSION.iso
+wget http://192.168.0.195/~eric/isos/VBoxGuestAdditions_4.1.2.iso
 mount -o loop VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
 sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
