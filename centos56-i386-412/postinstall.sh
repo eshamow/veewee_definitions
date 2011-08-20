813e36185b2aecdd501563366025257605806f44
Eric Shamow <eric@puppetlabs.com>
2011-08-20 00:52:47 -0400

    rebuilt 56

--- centos56-i386-412/postinstall.sh
+++ centos56-i386-412/postinstall.sh
@@ -2,22 +2,22 @@
 #kernel source is needed for vbox additions
 
 date > /etc/vagrant_box_build_time
-
 rpm -Uvh http://download.fedora.redhat.com/pub/epel/5/i386/epel-release-5-4.noarch.rpm
 
-yum -y install gcc bzip2 make kernel-devel-`uname -r` git
+yum -y install gcc bzip2 make kernel-devel-`uname -r`
 
 #yum -y update
 #yum -y upgrade
 
-yum -y install gcc-c++ zlib-devel openssl-devel readline-devel sqlite3-devel
+yum -y install gcc-c++ zlib-devel openssl-devel readline-devel sqlite3-devel git
 
 yum -y erase wireless-tools gtk2 libX11 hicolor-icon-theme avahi freetype bitstream-vera-fonts
+
 
 yum -y clean all
 
 #Installing ruby
-wget http://192.168.0.195/~eric/isos/ruby-enterprise-1.8.7-2010.02.tar.gz
+wget http://rubyforge.org/frs/download.php/71096/ruby-enterprise-1.8.7-2010.02.tar.gz
 tar xzvf ruby-enterprise-1.8.7-2010.02.tar.gz
 ./ruby-enterprise-1.8.7-2010.02/installer -a /opt/ruby --no-dev-docs --dont-install-useful-gems
 echo 'PATH=$PATH:/opt/ruby/bin'> /etc/profile.d/rubyenterprise.sh
@@ -28,51 +28,27 @@ rm ruby-enterprise-1.8.7-2010.02.tar.gz
 #/opt/ruby/bin/gem install chef --no-ri --no-rdoc
 #/opt/ruby/bin/gem install puppet --no-ri --no-rdoc
 
-# Install envpuppet
-cd /usr/local/src
-git clone git://github.com/puppetlabs/puppet.git
-git clone git://github.com/puppetlabs/facter.git
-pushd puppet
-git checkout tags/2.6.6
-popd
-pushd facter
-git checkout tags/1.5.8
-export ENVPUPPET_BASEDIR=/usr/local/src
-cd /usr/local/bin
-wget http://192.168.0.195/~eric/isos/envpuppet
-
-cat > puppet << EOF
-#! /bin/bash
-export ENVPUPPET_BASEDIR=/usr/local/src
-exec "/usr/local/bin/envpuppet ${ENVPUPPET_BASEDIR}"/puppet/bin/puppet $@
-EOF
-
-chmod +x puppet
-chmod +x envpuppet
-echo "export ENVPUPPET_BASEDIR=/usr/local/src" >> /etc/bashrc
-echo "alias puppet=\""envpuppet puppet\""" >> /etc/skel/bashrc
-echo "alias facter=\""envpuppet facter\""" >> /etc/skel/bashrc
-echo "alias puppet=\""envpuppet puppet\""" >> /root/.bashrc
-echo "alias facter=\""envpuppet facter\""" >> /root/.bashrc
-echo "alias puppet=\""envpuppet puppet\""" >> /home/vagrant/.bashrc
-echo "alias facter=\""envpuppet facter\""" >> /home/vagrant/.bashrc
+# Get envpuppet install script
+wget http://192.168.0.195/~eric/isos/puppetinst.sh
+sh puppetinst_debian.sh
 
 #Installing vagrant keys
 mkdir /home/vagrant/.ssh
 chmod 700 /home/vagrant/.ssh
 cd /home/vagrant/.ssh
-wget 'http://192.168.0.195/~eric/isos/vagrant.pub' -O authorized_keys
+wget --no-check-certificate 'http://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub' -O authorized_keys
 chown -R vagrant /home/vagrant/.ssh
 
 #Installing the virtualbox guest additions
 VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
 cd /tmp
-wget http://192.168.0.195/~eric/isos/VBoxGuestAdditions_4.1.2.iso
+wget http://download.virtualbox.org/virtualbox/$VBOX_VERSION/VBoxGuestAdditions_$VBOX_VERSION.iso
 mount -o loop VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
 sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
 
 rm VBoxGuestAdditions_$VBOX_VERSION.iso
+
 
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 
