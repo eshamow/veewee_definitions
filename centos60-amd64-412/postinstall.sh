bd72e49cf7fe9803ea953cbd32a3a1eeedf5a3ae
Eric Shamow <eric@puppetlabs.com>
2011-08-20 20:03:01 -0400

    centos 60 i386 working, added x86_64

---
+++ centos60-amd64-412/postinstall.sh
@@ -0,0 +1,42 @@
+#http://chrisadams.me.uk/2010/05/10/setting-up-a-centos-base-box-for-development-and-testing-with-vagrant/
+
+date > /etc/vagrant_box_build_time
+
+cat > /etc/yum.repos.d/epel.repo << EOM
+[epel]
+name=epel
+baseurl=http://download.fedoraproject.org/pub/epel/6/\$basearch
+enabled=1
+gpgcheck=0
+EOM
+
+yum -y install ruby-devel rubygems git
+yum -y erase wireless-tools gtk2 libX11 hicolor-icon-theme avahi freetype bitstream-vera-fonts
+yum -y clean all
+rm /etc/yum.repos.d/{puppetlabs,epel}.repo
+
+# Get envpuppet install script
+wget http://192.168.0.195/~eric/isos/puppetinst.sh
+sh puppetinst.sh
+
+# Installing vagrant keys
+mkdir /home/vagrant/.ssh
+chmod 700 /home/vagrant/.ssh
+cd /home/vagrant/.ssh
+wget --no-check-certificate 'http://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub' -O authorized_keys
+chown -R vagrant /home/vagrant/.ssh
+
+# Installing the virtualbox guest additions
+VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
+cd /tmp
+wget http://192.168.0.195/~eric/isos/VBoxGuestAdditions_4.1.2.iso
+mount -o loop VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+sh /mnt/VBoxLinuxAdditions.run
+umount /mnt
+rm VBoxGuestAdditions_$VBOX_VERSION.iso
+
+sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
+
+dd if=/dev/zero of=/tmp/clean || rm /tmp/clean
+
+exit
