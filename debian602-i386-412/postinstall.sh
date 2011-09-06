bdec048cafb1ae4b8c7765e8892bb4b5a2d0db5e
Eric Shamow <eric@puppetlabs.com>
2011-09-06 19:42:16 -0400

    Revised 6.0.2 for chef, added i386

---
+++ debian602-i386-412/postinstall.sh
@@ -0,0 +1,130 @@
+#http://adrianbravo.tumblr.com/post/644860401
+
+date > /etc/vagrant_box_build_time
+
+# Update the box
+apt-get -y update
+apt-get -y install linux-headers-$(uname -r) build-essential
+apt-get -y install zlib1g-dev libssl-dev libreadline5-dev
+apt-get -y install curl unzip
+apt-get clean
+
+# Set up sudo
+cp /etc/sudoers /etc/sudoers.orig
+sed -i -e 's/%sudo ALL=(ALL) ALL/%sudo ALL=NOPASSWD:ALL/g' /etc/sudoers
+
+
+#Installing ruby
+apt-get -y install ruby ruby1.8-dev libopenssl-ruby1.8 rdoc ri irb make g++ libshadow-ruby1.8 rubygems
+
+#Installing prerequisite utilities
+apt-get -y install git-core lsb-release 
+
+# Install RubyGems 1.7.2
+#wget http://production.cf.rubygems.org/rubygems/rubygems-1.7.2.tgz
+#tar xzf rubygems-1.7.2.tgz
+#cd rubygems-1.7.2
+#/usr/bin/ruby setup.rb
+#cd ..
+#rm -rf rubygems-1.7.2*
+#ln -sfv /usr/bin/gem1.8 /usr/bin/gem
+
+# Install envpuppet
+cd /usr/local/src
+git clone git://github.com/puppetlabs/puppet.git
+git clone git://github.com/puppetlabs/facter.git
+cd puppet
+git checkout tags/2.6.9
+cd ../facter
+git checkout tags/1.5.8
+export ENVPUPPET_BASEDIR=/usr/local/src
+cd /usr/local/bin
+
+#cp $ENVPUPPET_BASEDIR/puppet/ext/envpuppet .
+wget https://raw.github.com/jeffmccune/puppet/feature/2.6.x/6395/ext/envpuppet
+
+cat > /usr/local/bin/puppet << EOF
+#! /bin/bash
+export ENVPUPPET_BASEDIR=/usr/local/src
+eval \$(envpuppet)
+exec "\${ENVPUPPET_BASEDIR}"/puppet/bin/puppet \$@
+EOF
+
+cat > /usr/local/bin/facter << EOF
+#! /bin/bash
+export ENVPUPPET_BASEDIR=/usr/local/src
+eval \$(envpuppet)
+exec "\${ENVPUPPET_BASEDIR}"/facter/bin/facter \$@
+EOF
+
+chmod +x /usr/local/bin/puppet
+chmod +x /usr/local/bin/facter
+chmod +x /usr/local/bin/envpuppet
+echo "export ENVPUPPET_BASEDIR=/usr/local/src" >> /etc/bashrc
+
+## Install chef
+#Add OpsCode deb repository
+sudo mkdir -p /etc/apt/trusted.gpg.d
+sudo gpg --keyserver keys.gnupg.net --recv-keys 83EF826A
+echo "deb http://apt.opscode.com/ `lsb_release -cs`-0.10 main" | sudo tee /etc/apt/sources.list.d/opscode.list
+sudo gpg --export packages@opscode.com | sudo tee /etc/apt/trusted.gpg.d/opscode-keyring.gpg > /dev/null
+sudo apt-get update
+sudo apt-get install opscode-keyring # permanent upgradeable keyring
+
+#/opt/ruby/bin/gem install chef --no-ri --no-rdoc
+export DEBIAN_FRONTEND=noninteractive
+apt-get -q -y install chef
+
+# Install vagrant keys
+mkdir -p /home/vagrant/.ssh
+chmod 700 /home/vagrant/.ssh
+cd /home/vagrant/.ssh
+curl -o /home/vagrant/.ssh/authorized_keys \
+  'http://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub'
+chmod 0600 /home/vagrant/.ssh/authorized_keys
+chown -R vagrant:vagrant /home/vagrant/.ssh
+
+# Tweak sshd to prevent DNS resolution (speed up logins)
+echo 'UseDNS no' >> /etc/ssh/sshd_config
+
+# Customize the message of the day
+echo 'Welcome to your Vagrant-built virtual machine.' > /var/run/motd
+
+# The netboot installs the VirtualBox support (old) so we have to remove it
+/etc/init.d/virtualbox-ose-guest-utils stop
+rmmod vboxguest
+aptitude -y purge virtualbox-ose-guest-x11 virtualbox-ose-guest-dkms virtualbox-ose-guest-utils
+
+# Install the VirtualBox guest additions
+VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
+curl -Lo /tmp/VBoxGuestAdditions_$VBOX_VERSION.iso \
+  "http://download.virtualbox.org/virtualbox/$VBOX_VERSION/VBoxGuestAdditions_$VBOX_VERSION.iso"
+mount -o loop /tmp/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+yes|sh /mnt/VBoxLinuxAdditions.run
+umount /mnt
+
+# Clean up
+apt-get -y remove linux-headers-$(uname -r) build-essential
+apt-get -y autoremove
+
+rm /tmp/VBoxGuestAdditions_$VBOX_VERSION.iso 
+
+# Zero out the free space to save space in the final image:
+dd if=/dev/zero of=/EMPTY bs=1M
+rm -f /EMPTY
+
+# Removing leftover leases and persistent rules
+echo "cleaning up dhcp leases"
+rm /var/lib/dhcp3/*
+
+# Make sure Udev doesn't block our network
+# http://6.ptmc.org/?p=164
+echo "cleaning up udev rules"
+rm /etc/udev/rules.d/70-persistent-net.rules
+mkdir /etc/udev/rules.d/70-persistent-net.rules
+rm -rf /dev/.udev/
+rm /lib/udev/rules.d/75-persistent-net-generator.rules
+
+echo "Adding a 2 sec delay to the interface up, to make the dhclient happy"
+echo "pre-up sleep 2" >> /etc/network/interfaces
+exit
