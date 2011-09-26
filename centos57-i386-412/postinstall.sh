2dd639114b7d69829cbe348bf5a6ea4cfc169f43
Eric Shamow <eric@puppetlabs.com>
2011-09-25 22:13:52 -0400

    Restructured templates

--- centos57-i386-412/postinstall.sh
+++ centos57-i386-412/postinstall.sh
@@ -12,51 +12,12 @@ rpm -Uvh http://download.fedora.redhat.com/pub/epel/5/i386/epel-release-5-4.noar
 
 #kernel source is needed for vbox additions
 yum -y install gcc bzip2 make kernel-devel-`uname -r`
-#yum -y update
-#yum -y upgrade
+yum -y update
+yum -y upgrade
 
 yum -y install gcc-c++ zlib-devel openssl-devel readline-devel sqlite3-devel git
 yum -y erase wireless-tools gtk2 libX11 hicolor-icon-theme avahi freetype bitstream-vera-fonts
 yum -y clean all
-
-#Install Ruby 1.8.7
-cd /etc/yum.repos.d
-wget http://centos.karan.org/el5/ruby187/kbs-el5-ruby187.repo
-yum clean all
-yum -y install ruby rubygems
-
-# Install envpuppet
-cd /usr/local/src
-git clone git://github.com/puppetlabs/puppet.git
-git clone git://github.com/puppetlabs/facter.git
-cd puppet
-git checkout tags/2.6.9
-cd ../facter
-git checkout tags/1.5.8
-export ENVPUPPET_BASEDIR=/usr/local/src
-cd /usr/bin
-
-#cp $ENVPUPPET_BASEDIR/puppet/ext/envpuppet .
-wget https://raw.github.com/jeffmccune/puppet/feature/2.6.x/6395/ext/envpuppet
-
-cat > /usr/bin/puppet << EOF
-#! /bin/bash
-export ENVPUPPET_BASEDIR=/usr/local/src
-eval \$(envpuppet)
-exec "\${ENVPUPPET_BASEDIR}"/puppet/bin/puppet \$@
-EOF
-
-cat > /usr/bin/facter << EOF
-#! /bin/bash
-export ENVPUPPET_BASEDIR=/usr/local/src
-eval \$(envpuppet)
-exec "\${ENVPUPPET_BASEDIR}"/facter/bin/facter \$@
-EOF
-
-chmod +x /usr/bin/puppet
-chmod +x /usr/bin/facter
-chmod +x /usr/bin/envpuppet
-echo "export ENVPUPPET_BASEDIR=/usr/local/src" >> /etc/bashrc
 
 #Installing vagrant keys
 mkdir /home/vagrant/.ssh
@@ -68,7 +29,8 @@ chown -R vagrant /home/vagrant/.ssh
 #Installing the virtualbox guest additions
 VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
 cd /tmp
-wget http://download.virtualbox.org/virtualbox/$VBOX_VERSION/VBoxGuestAdditions_$VBOX_VERSION.iso
+#wget http://download.virtualbox.org/virtualbox/$VBOX_VERSION/VBoxGuestAdditions_$VBOX_VERSION.iso
+wget http://172.19.131.103/~eric/isos/VBoxGuestAdditions_4.1.2.iso
 mount -o loop VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
 sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
