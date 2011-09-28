06d53b35a0e0e4e59ca3b38bf7627403941c32e6
Eric Shamow <eric@puppetlabs.com>
2011-09-28 00:14:50 -0400

    Added ubuntu 11.04

--- ubuntu10043-i386-412/postinstall.sh
+++ ubuntu10043-i386-412/postinstall.sh
@@ -18,48 +18,6 @@ sed -i -e 's/%admin ALL=(ALL) ALL/%admin ALL=NOPASSWD:ALL/g' /etc/sudoers
 # Install NFS client
 apt-get -y install nfs-common
 
-
-# Install Ruby 1.8.7
-apt-get -y  install ruby1.8 rubygems ruby git-core libopenssl-ruby
-
-# Install envpuppet
-cd /usr/local/src
-git clone git://github.com/puppetlabs/puppet.git
-git clone git://github.com/puppetlabs/facter.git
-cd puppet
-git checkout tags/2.6.9
-cd ../facter
-git checkout tags/1.5.8
-export ENVPUPPET_BASEDIR=/usr/local/src
-cd /usr/bin
-
-#cp $ENVPUPPET_BASEDIR/puppet/ext/envpuppet .
-wget https://raw.github.com/jeffmccune/puppet/feature/2.6.x/6395/ext/envpuppet
-
-cat > /usr/bin/puppet << EOF
-#! /bin/bash
-export ENVPUPPET_BASEDIR=/usr/local/src
-eval \$(envpuppet)
-exec "\${ENVPUPPET_BASEDIR}"/puppet/bin/puppet \$@
-EOF
-
-cat > /usr/bin/facter << EOF
-#! /bin/bash
-export ENVPUPPET_BASEDIR=/usr/local/src
-eval \$(envpuppet)
-exec "\${ENVPUPPET_BASEDIR}"/facter/bin/facter \$@
-EOF
-
-chmod +x /usr/bin/puppet
-chmod +x /usr/bin/facter
-chmod +x /usr/bin/envpuppet
-echo "export ENVPUPPET_BASEDIR=/usr/local/src" >> /etc/bashrc
-
-## Install chef
-#/opt/ruby/bin/gem install chef --no-ri --no-rdoc
-export DEBIAN_FRONTEND=noninteractive
-apt-get -q -y install chef
-
 # Installing vagrant keys
 mkdir /home/vagrant/.ssh
 chmod 700 /home/vagrant/.ssh
