d068393d309dd66f20c466cb7ddd587d25d49c14
Eric Shamow <eric@puppetlabs.com>
2011-10-18 12:28:41 -0400

    Renamed centos definitions to be VBox version-agnostic Fixed centos definitions to match new network config for veewee

--- centos57-amd64/definition.rb
+++ centos57-amd64/definition.rb
@@ -6,11 +6,12 @@ Veewee::Session.declare({
   :iso_src => "http://mirror.ukhost4u.com/centos/5.7/isos/x86_64/CentOS-5.7-x86_64-bin-DVD/CentOS-5.7-x86_64-bin-DVD-1of2.iso",
   :iso_md5 => "02cf3a5e32aaa5eed27af775ad292beb",
   :iso_download_timeout => 1000,
-  :boot_wait => "10", :boot_cmd_sequence => [ 'linux text ks=http://%IP%:%PORT%/ks.cfg<Enter>' ],
+  :boot_wait => "10", :boot_cmd_sequence => [ 'linux text ks=http://%IP%:%PORT%/ks.cfg ksdevice=eth0<Enter> ' ],
   :kickstart_port => "7122", :kickstart_timeout => 10000, :kickstart_file => "ks.cfg",
   :ssh_login_timeout => "100", :ssh_user => "vagrant", :ssh_password => "vagrant", :ssh_key => "",
   :ssh_host_port => "7222", :ssh_guest_port => "22",
   :sudo_cmd => "echo '%p'|sudo -S sh '%f'",
   :shutdown_cmd => "/sbin/halt -h -p",
-  :postinstall_files => [ "postinstall.sh"], :postinstall_timeout => 10000
+  :postinstall_files => [ "postinstall.sh"], :postinstall_timeout => 10000,
+  :interfaces => ["nat","hostonly"],:hostonly_network => "vboxnet0"
 })
